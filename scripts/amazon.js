4ce99799683d51f14fbfab7c097c05d2e8a55ab1
kigila <ifalmeidx@gmail.com>
2024-06-15 14:51:32 +0100

    Make the select quantiy dinamic

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach(product => {
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -61,6 +61,7 @@ document.querySelector('.js-products-grid').innerHTML = productHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
+    const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
 
     let matchingItem;
 
@@ -71,11 +72,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     if(matchingItem){
-      matchingItem.quantity++;
+      matchingItem.quantity += quantity;
     }else{
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: quantity
       });
     }
 
@@ -83,6 +84,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     cart.forEach((item)=>{
       cartQuantity += item.quantity;
     })
+
+    console.log(cart)
 
     document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
   });
