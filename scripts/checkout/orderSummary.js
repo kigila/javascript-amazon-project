f408b9c04fa2186eda441413e90702c29dd479cd
kigila <ifalmeidx@gmail.com>
2024-06-19 15:04:30 +0100

    Create automated test for order summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -25,7 +25,7 @@ export function renderOrderSummary(){
     const dateString=calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML +=`
-    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+    <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: ${dateString}
       </div>
@@ -41,7 +41,7 @@ export function renderOrderSummary(){
           <div class="product-price">
             ${formatCurrency(matchingProduct.priceCents)}
           </div>
-          <div class="product-quantity">
+          <div class="product-quantity js-product-quantity-${matchingProduct.id}">
             <span>
               Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
@@ -52,7 +52,7 @@ export function renderOrderSummary(){
             <input id="number" type="number" min="1" max="1000" inputmode="numeric" class="quantity-input js-quantity-input-${matchingProduct.id}">
             <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
 
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+            <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
               Delete
             </span>
             <p class="js-wrong-input-ms-${matchingProduct.id}" style="Color:red;padding:1px;font-size:15px;"></p>
